8e34294e6f52019abbe6ac966efc5b576e2ae475
Villem Heinsalu <villem.heinsalu@gmail.com>
2018-08-23 12:14:06 +0300

    uus

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var teenusedRouter = require('./routes/teenused');
+var kontaktRouter = require('./routes/kontakt');
 
 var app = express();
 
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/teenused', teenusedRouter);
+app.use('/kontakt', kontaktRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
