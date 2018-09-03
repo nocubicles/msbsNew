7322e27e35db730995178906c2fc933d71fe0185
unknown <villem.heinsalu@gmail.com>
2018-09-03 13:56:56 +0300

    uuendused

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var kontaktRouter = require('./routes/kontakt');
+var clouderp = require('./routes/clouderp');
+
 
 var app = express();
 
@@ -22,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/kontakt', kontaktRouter);
+app.use('/clouderp', clouderp);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
